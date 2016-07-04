6ec14950320799867f64481b8c80916dee16a2a4
vigiro <vidal_romero@hotmail.com>
2016-07-04 12:50:00 -0500

    ham

--- hamburguesas/ViewController.swift
+++ hamburguesas/ViewController.swift
@@ -4,12 +4,19 @@
 //
 //  Created by vidal romero  on 29/06/16.
 //  Copyright © 2016 vidal romero . All rights reserved.
-//
+// Cambio 1
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var cambioPais: UILabel!
+    
+    @IBOutlet weak var cambioHamburguesa: UILabel!
+    let pais = ColeccionDePaises()
+    let hamburguesa = ColeccionDeHamburguesa()
+    let colores = Colores()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +27,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func cambioCombinacion() {
+        cambioPais.text = pais.obtenPais()
+        cambioHamburguesa.text = hamburguesa.obtenHamburguesa()
+        let colorAleatorio = colores.regresaColorAleatorio()
+        view.backgroundColor = colorAleatorio
+    }
 
 }
 
